d948a7deb1cc9062400444c3520811ae26ec51c6
ayyoubez <ayyoubez@gmail.com>
2024-01-08 02:47:58 -0800

    data

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_1 = ("None",)
+    tuple_1 = ()
     if len(sentence) == 0:
-        return tuple_1[0]
+        tuple_1 = (0, "None")
     else:
-        return (len(sentence)), sentence[0]
+        tuple_1 = (len(sentence), sentence[0])
+    return tuple_1
