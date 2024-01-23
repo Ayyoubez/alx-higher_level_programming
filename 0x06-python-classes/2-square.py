bfe4eb215761d1bc4eba12f5c1c01272d1d9c459
ayyoubez <ayyoubez@gmail.com>
2024-01-23 06:35:23 -0800

    OOP

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-""" Square Module """
+"""Square Module """
 
 
 class Square:
     """ Square class """
 
     def __init__(self, size=0)
-        """ constructor 
+        """ constructor
+
         args:
             size (int): The size of the square
         """
