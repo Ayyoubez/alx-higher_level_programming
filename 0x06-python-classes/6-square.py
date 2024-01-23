b2f65bd4b7737765107b2729f04c6125d6cb2414
ayyoubez <ayyoubez@gmail.com>
2024-01-23 14:29:04 -0800

    OOP

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@
 class Square:
     """ Square definition """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0))):
         """ Sqaure Initialisation
 
         Args:
