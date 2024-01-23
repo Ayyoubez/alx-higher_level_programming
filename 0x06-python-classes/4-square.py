f4c6cfeece03add304e13e1ed803183fadfaeaad
ayyoubez <ayyoubez@gmail.com>
2024-01-23 07:23:51 -0800

    OOP

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,17 +13,13 @@ class Square:
             size (int): the size of the square side
         """
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
-        @property
         def area(self):
             """ return area """
             return (self.__size ** 2)
 
+        @property
         def size(self):
             """ retrive the size """
             return self.__size
