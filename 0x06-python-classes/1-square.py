6bb2f9954fed200179c7bd178429566b5619fda2
ayyoubez <ayyoubez@gmail.com>
2024-01-23 06:05:35 -0800

    OOP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,12 @@
 
 class Square:
     """ class definition """
+
     def __init__(self, size):
         """ constructor
 
         args:
             size: length of the square side
         """
+
         self.__size = size
