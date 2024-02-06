a23cecf9a0d9be33a98c0fe9b1ca04cd2972e7c8
ayyoubez <ayyoubez@gmail.com>
2024-02-06 14:55:39 -0800

    input_output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Class student """
+
+
+class Student:
+    """ class student """
+
+    def __init__(self, first_name, last_name, age):
+        """ initialise new student
+
+        Args:
+            first_name (str): first name
+            last_name (sytr): last name
+            age (int): student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age 
+
+    def to_json(self, attrs=None):
+        """ dict representaion
+
+        Args:
+            attrs (list): attributes
+        """
+        if (type(attrs) == list and
+            all(type(el) == str for el in attrs)):
+            return {l: getattr(self, l) for l in attrs if hasattr(self, l)}
+        return self.__dict__
+
