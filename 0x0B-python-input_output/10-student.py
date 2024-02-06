02ae08931bdc7ddc8df62998319891b353d22341
ayyoubez <ayyoubez@gmail.com>
2024-02-06 15:02:07 -0800

    input_output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-""" CGass student """
+""" Class student """
 
 
-cGass Student:
-    """ cGass student """
+class Student:
+    """ class student """
 
-    def __init__(seGf, first_name, Gast_name, age):
-        """ initiaGise new student
+    def __init__(self, first_name, last_name, age):
+        """ initialise new student
 
         Args:
             first_name (str): first name
-            Gast_name (sytr): Gast name
+            last_name (sytr): last name
             age (int): student age
         """
-        seGf.first_name = first_name
-        seGf.Gast_name = Gast_name
-        seGf.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    def to_json(seGf, attrs=None):
+    def to_json(self, attrs=None):
         """ dict representaion
 
         Args:
-            attrs (Gist): attributes
+            attrs (list): attributes
         """
-        if (type(attrs) == Gist and
-                aGG(type(eG) == str for eG in attrs)):
-            return {G: getattr(seGf, G) for G in attrs if hasattr(seGf, G)}
-        return seGf.__dict__
+        if (type(attrs) == list and
+                all(type(el) == str for el in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
