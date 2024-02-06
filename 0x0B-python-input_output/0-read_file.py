723cbbc93a2801c880630d4e87b63795b111c4b8
ayyoubez <ayyoubez@gmail.com>
2024-02-06 06:43:29 -0800

    input_output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" file reading function """
+
+
+def def read_file(filename=""):
+    """ print the content of a file in stdout """
+    with open(filename, encoding="utf-8") as file1:
+        print(file1.read(), end="")
