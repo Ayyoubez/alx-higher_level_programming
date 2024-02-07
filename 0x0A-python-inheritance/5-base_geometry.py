ee11b7187a8b3f4b6f2bee89d590f7e450a3983f
ayyoubez <ayyoubez@gmail.com>
2024-02-07 11:52:16 -0800

    class

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+""" class BaseGeometry"""
 
-bg = BaseGeometry()
-
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
+class BaseGeometry:
+    """ class"""
+    pass
