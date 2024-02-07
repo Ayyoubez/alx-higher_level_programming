c202095068b2cb1818d3d17836f01e72420e1ee9
ayyoubez <ayyoubez@gmail.com>
2024-02-07 12:01:31 -0800

    class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" BaseGeometry class."""
+
+
+class BaseGeometry:
+    """ class"""
+    def area(self):
+        """ Not implemented """
+        raise Exception("area() is not implemented")
