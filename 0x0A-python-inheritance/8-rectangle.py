cd0740b61c87f11fbfdd9667f4dca3c056e36c65
ayyoubez <ayyoubez@gmail.com>
2024-02-07 12:29:12 -0800

    class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Rectangle inherits from Bsegeometry"""
+BaseGeometry = --import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ rectangle inherit BaseGeo"""
+
+    def __init(self, width, height):
+        """ initialisation
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
