64d31d02299dc85c97f471c89d17e8984e51bae0
Ayyoubez <Ayyoubez@gmail.com>
2024-02-12 20:46:21 +0100

    Python is circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,6 +18,29 @@ class TestRectangle_inst(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle()
 
-    def test_no_arg(self):
+    def test_1_arg(self):
         with self.assertRaises(TypeError):
             Rectangle(1)
+
+    def test_2_arg(self):
+        rec1 = Rectangle(10, 5)
+        rec2 = Rectangle(5, 10)
+        self.assertEqual(rec1.id, rec2.id - 1)
+
+    def test_3_arg(self):
+        rec1 = Rectangle(4, 4, 8)
+        rec2 = Rectangle(8, 8, 4))
+        self.assertEqual(rec1.id, rec2.id - 1)
+
+    def test_2_arg(self):
+        rec1 = Rectangle(1, 2, 3, 4)
+        rec2 = Rectangle(4, 3, 2, 1)
+        self.assertEqual(rec1.id, rec2.id - 1)
+
+    def test_5_arg(self):
+        self.asserEqual(7, REctangle(10, 2, 0, 0, 7).id)
+
+    def test_more_arg(self):
+        with self.assertRaises(AttributeError):
+            Rectangle(1, 2, 3, 4, 5, 6)
+
