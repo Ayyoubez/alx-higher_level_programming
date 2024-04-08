3119295c862d12638d116a6791f135f93db36457
ayyoubez <ayyoubez@gmail.com>
2024-04-08 16:38:50 +0000

    JS

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+let arguments = process.argv;
+if (arguments.length === 2)
+{
+	console.log('No argument');
+}
+else if (arguments.length === 3)
+{
+	console.log('Argument found');
+}
+else
+	console.log('Arguments found');
+
