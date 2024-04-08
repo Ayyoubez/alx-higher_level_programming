8cc09aea9b380af36996dfb112e55ca7a75598af
ayyoubez <ayyoubez@gmail.com>
2024-04-08 17:53:42 +0000

    JS

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,10 @@
 
 const args = process.argv;
 
-if (isNaN(args[2]) || args[2] == null)
-{
-	console.log("Missing size");
+if (isNaN(args[2]) || args[2] == null) {
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < args[2]; i++)
-	{
-			console.log("X".repeat(args[2]));
-	}}
-
-
+  for (let i = 0; i < args[2]; i++) {
+    console.log('X'.repeat(args[2]));
+  }
+}
