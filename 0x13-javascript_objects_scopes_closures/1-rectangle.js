b0cf0ce181278e424d588bacad9769c203b3a0eb
ayyoubez <ayyoubez@gmail.com>
2024-04-09 17:14:02 +0000

    JS scope and closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constrcutor(w, h)
-	{
-		this.width = w;
-		this.height = h;
-	}}
+  constrcutor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
 module.exports = Rectangle;
