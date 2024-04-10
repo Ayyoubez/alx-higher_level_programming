6b2ac6898ec35e06396e89bbd70bc56b9e6cc566
ayyoubez <ayyoubez@gmail.com>
2024-04-10 00:41:47 +0000

    JS scope and closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,16 +3,12 @@
 const SquareL = require('./5-square');
 
 class Square extends SquareL {
-  charprint (c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+      console.log(c.repeat(this.width));
     }
   }
 }
