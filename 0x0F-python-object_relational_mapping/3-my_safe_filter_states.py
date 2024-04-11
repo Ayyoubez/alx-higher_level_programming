d02f8b7104bdd8535b0804fdcecc1c4c46324b35
ayyoubez <ayyoubez@gmail.com>
2024-04-11 10:53:03 +0000

    ORM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 """  lists all states from the database hbtn_0e_0_usa with filter """
-
 import MySQLdb
 from sys import argv
 
@@ -9,11 +8,10 @@ if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", user=argv[1],
                                  password=argv[2], db=argv[3], port=3306)
     cursor = connection.cursor()
-
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4]))
+    valid = argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (valid, ))
                 .format(sys.argv[4]))
-    data= cursor.fetchall()
+    data = cursor.fetchall()
 
     for row in data:
         print(row)
