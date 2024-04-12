8e53ec8eb6356063c42a86de0923c1d121bfd00b
ayyoubez <ayyoubez@gmail.com>
2024-04-12 09:09:14 +0000

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                  password=argv[2], db=argv[3], port=3306)
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s '{}'"
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                    .format(argv[4]))
     data = cursor.fetchall()
 
