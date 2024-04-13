cd93dd3006a3163afcb9ca4597b0e86fcb6f1455
ayyoubez <ayyoubez@gmail.com>
2024-04-13 09:30:38 +0000

    ORM

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,7 +7,7 @@ from sys import argv
 from sqlalchemy.orm import sessionmaker
 
 
-if ___name__ == "__main__":
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
