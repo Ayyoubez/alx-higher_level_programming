4cfe0feac35ba7d81f3e2ebf52cd7badc978e988
ayyoubez <ayyoubez@gmail.com>
2024-04-13 06:51:39 +0000

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-state class and base instance 
-"""
+
+""" state class and base instance """
+
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -18,4 +18,3 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
