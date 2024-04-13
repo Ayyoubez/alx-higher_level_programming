56128fd4dcc63e2ab652be53e887a28e7724b2ba
ayyoubez <ayyoubez@gmail.com>
2024-04-13 07:04:24 +0000

    ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" state class and base instance"""
+""" state class and base instance of decarative base () """
 
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
