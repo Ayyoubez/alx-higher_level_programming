9eeefc01653385fdf15350a8dbbadb9a62b4190d
ayyoubez <ayyoubez@gmail.com>
2024-04-13 09:18:36 +0000

    ORM

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Lists all the states staring with letter a
+"""
+
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+if ___name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.creat_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for inst in session.query(State).filter(State.name.like('%a%')):
+        print(inst.id, inst.name, sep=": ")
