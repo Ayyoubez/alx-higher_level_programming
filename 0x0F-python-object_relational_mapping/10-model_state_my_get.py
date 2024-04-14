6f8bf2b43f27920fdd9071c166ada8102412e0fe
ayyoubez <ayyoubez@gmail.com>
2024-04-14 07:30:39 +0100

    ORM

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Prints the sate with name passed as an argument
+"""Prints the sate with name passed as an argument from the database 
 """
 from model_state import Base, State
 from sqlalchemy import (create_engine)
