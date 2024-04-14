dc6cbf1bda9ac6335c6baa7de5bfc63b2a22f834
ayyoubez <ayyoubez@gmail.com>
2024-04-14 07:40:35 +0100

    ORM

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/pyuthon3
 
-"""adds the states louisiana to the database
-"""
+"""adds the states louisiana to the database """
+
 from sys import argv
 from sqlalchemy import create_engine
 from model_state import Base, State
@@ -9,7 +9,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306:/{}"
-                           .format(argv[1], argv[2], argv[3])
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind+engine)
     session = Session()
