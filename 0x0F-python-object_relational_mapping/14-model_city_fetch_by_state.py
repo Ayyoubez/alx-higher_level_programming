de46d83cc4599ed144668a7a1d14e1d7f7008126
ayyoubez <ayyoubez@gmail.com>
2024-04-14 08:08:48 +0100

    ORM

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for inst in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
+                 .filter(State.id == City.state_id)):
         print(inst[0] + ": (" + str(inst[1]) + ") " + inst[2])
