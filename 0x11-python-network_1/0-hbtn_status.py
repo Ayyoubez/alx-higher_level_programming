017ba06b073cb16517c15eb90129ba32957088e0
ayyoubez <ayyoubez@gmail.com>
2024-04-27 00:24:15 +0100

    networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that 
+"""A script that
 - fetching  https://alx-intranet.hbtn.io/status.
 - using urlib package to process
 """
@@ -8,8 +8,8 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
